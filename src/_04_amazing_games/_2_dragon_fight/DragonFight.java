4aa102d6b94df81af8eecf97199bb3daa697c81f
league <league@imac-24.attlocal.net>
2022-12-02 16:56:27 -0800

    i worked on dragons vs player game

--- src/_04_amazing_games/_2_dragon_fight/DragonFight.java
+++ src/_04_amazing_games/_2_dragon_fight/DragonFight.java
@@ -14,19 +14,19 @@ public class DragonFight {
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 
 		// 1. Create some variables to hold health levels
-		
+		int playerHealth;
 			// playerHealth to store your health - set it equal to 100
-	
+		playerHealth=100;
 			// dragonHealth to store the dragon's health - set it equal to 100
-
+		int dragonHealth=100;
 		// 2. Create some variables to hold the attack strengths. These will be given different values later. 
-		
+		int attackStrength;
 			// playerAttack to store the damage the player's attack will do - set it equal
 			// to 0 for now.
-	
+		int playerAttack=0;
 			// dragonAttack to store the damage the dragon's attack will do - set it equal
 			// to 0 for now.
-
+		int dragonAttack=0;
 		
 		//  This while statement will cause the game attack code to repeat
 		while (true) {
@@ -35,12 +35,11 @@ public class DragonFight {
 
 				// 3. Ask the player in a pop-up if they want to attack the dragon with a yell
 				// or a kick
-	
+			JOptionPane.showInputDialog("would you like to attack the dragon with a yell or a kick?");
 				// 4. If they typed in "yell":
-	
 					  // -- Find a random number between 0 and 10 and store it in playerAttack. Use
 					  // ran.nextInt(10)
-			
+			ran.nextInt(10);
 				// 5. If they typed in "kick":
 	
 					  // -- Find a random number between 0 and 25 and store it in playerAttack.
